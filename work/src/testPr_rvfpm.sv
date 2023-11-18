688e56f6886a0ba8206b4b2a34dc0c09e192e8ba
andreasbakke <andreas.s.bakke@gmail.com>
2023-11-18 20:54:18 +0100

    Fixed testing

--- work/src/testPr_rvfpm.sv
+++ work/src/testPr_rvfpm.sv
@@ -12,8 +12,11 @@ program automatic testPr_rvfpm #(
         init();
         fillRF();
         basic();
-        // testLoad();
-        // testRead();
+        repeat(100) testRTYPE(); //TODO: do we need to seed random?
+        testRTYPE(.rd(19)); //Test with specified destination
+        @(posedge uin_rvfpm.ck);
+        uin_rvfpm.instruction = 0;
+        repeat(PIPELINE_STAGES*2) @(posedge uin_rvfpm.ck);
         $finish;
     end
 
@@ -43,22 +46,33 @@ program automatic testPr_rvfpm #(
         uin_rvfpm.instruction[14:12] = 0; //W
         uin_rvfpm.instruction[11:7] = 0;  //rd (dest)
         uin_rvfpm.instruction[6:0] = 7'b0000111;  //OPCODE
-        for (int i=1; i<NUM_REGS; ++i) begin
-            @(posedge uin_rvfpm.ck)
+        for (int i=0; i<NUM_REGS; ++i) begin 
             uin_rvfpm.instruction[11:7] = i; //set register
             fork
-                repeat(PIPELINE_STAGES) @(posedge uin_rvfpm.ck);
-                uin_rvfpm.data_fromMem = $random; //set to random real at the appropriate time
+                begin
+                    repeat(PIPELINE_STAGES) @(posedge uin_rvfpm.ck);
+                    uin_rvfpm.data_fromMem = $random; //set to random real at the appropriate time
+                end
             join_none
+            @(posedge uin_rvfpm.ck);
         end
-        @(posedge uin_rvfpm.ck)
         uin_rvfpm.instruction = 0;
         repeat(PIPELINE_STAGES) @(posedge uin_rvfpm.ck); //wait for all operations to finish
-        uin_rvfpm.data_fromMem = 0; //set to random real //for last register (if pipelined by 1.)
+        uin_rvfpm.data_fromMem = 0;
     endtask
 
-    task setFromMem();
-
+    task testRTYPE(input int funct5 = 0, input int rs2 = $urandom_range(0, NUM_REGS-1), input int rs1 = $urandom_range(0, NUM_REGS-1), input int rd = $urandom_range(0, NUM_REGS-1));
+        @(posedge uin_rvfpm.ck)
+        uin_rvfpm.instruction[31:28] = funct5;
+        uin_rvfpm.instruction[26:25] = 0; //fmt
+        uin_rvfpm.instruction[24:20] = rs2; //rs2
+        uin_rvfpm.instruction[19:15] = rs1; //rs1 (base)
+        uin_rvfpm.instruction[14:12] = 0; //RM
+        uin_rvfpm.instruction[11:7] = rd;  //rd (dest)
+        uin_rvfpm.instruction[6:0] = 7'b1010011;  //OPCODE
+        $display(rs2);
+        $display(funct5);
+        $display(rd);
     endtask
 
     task basic();
@@ -68,32 +82,30 @@ program automatic testPr_rvfpm #(
         @(posedge uin_rvfpm.ck);
         uin_rvfpm.instruction = 32'b0000000_00000_00000_010_00001_0000111; //load from mem into register1;
         fork
+            begin
                 repeat(PIPELINE_STAGES) @(posedge uin_rvfpm.ck);
                 uin_rvfpm.data_fromMem = 1.7; //Data for the previous instruction (1 pipeline stage)
+            end
         join_none
 
         @(posedge uin_rvfpm.ck);
         uin_rvfpm.instruction = 32'b0000000_00000_00000_010_00010_0000111;; //load from mem into register2;
         fork
+            begin
                 repeat(PIPELINE_STAGES) @(posedge uin_rvfpm.ck);
                 uin_rvfpm.data_fromMem = 11.4; //Data for the previous instruction
+            end
         join_none
 
 
         @(posedge uin_rvfpm.ck);
         uin_rvfpm.instruction = 32'b0000000_00010_00001_000_00011_1010011; //Add r1 r2 and store in r3
         fork
+            begin
                 repeat(PIPELINE_STAGES) @(posedge uin_rvfpm.ck);
                 uin_rvfpm.data_fromMem = 0; //set back to 0
-                
+            end
         join_none
-
-        @(posedge uin_rvfpm.ck);
-        uin_rvfpm.instruction = 0;
-        #200;
-        uin_rvfpm.instruction = 32'b0000000_00000_00011_010_00011_0100111; //store r3 value to memory;
-        @(posedge uin_rvfpm.ck);
-        uin_rvfpm.instruction = 0;
     endtask
 
 endprogram
