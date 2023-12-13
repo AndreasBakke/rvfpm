ce23a9d9ecb435f785d8c8fdcd0e6aa709921c94
anb7 <andreas.bakke@nordicsemi.no>
2023-12-13 10:34:15 +0100

    finished resultparser

--- work/tests/result_parser.py
+++ work/tests/result_parser.py
@@ -14,17 +14,57 @@ def get_results(arch):
                 results[test_dir][file[:-4]] = extract_errors(file_path)
             except Exception as e:
                 print(f"Error reading {filename}: {e}")
-            # print(results)
+    summarize_tests(results)
 def extract_errors(path):
     with open(path, 'r', encoding='utf-8') as file:
+        tests_performed = 0
+        errors = 0
         line = file.readlines()[-1] #get last line
-        print(path + re.search(r"([1-9]+) tests", line))
-        tests_performed = 0#TODO:regex extraction   In 46464 tests, no errors found in f32_div, rounding near_even.  /  6133248 tests performed; 5964 errors found.
-        errors = 0 #TODO:regex extraction  
+        t = re.search(r'([0-9]+)\ tests', line)
+        if t != None:
+            tests_performed = t.groups()[0]
+        e = re.search(r'([0-9]+)\ errors found', line)
+        if e != None:
+            errors= e.groups()[0]
+        
         return {"tests_performed": tests_performed, "errors": errors}
 
 def summarize_tests(results):
-    print("haw")
+    instructions_passed = 0
+    instructions_failed = 0
+    failed_tests = {}
+    total_tests_performed = 0
+    total_tests_failed = 0
+    for rm in results:
+        failed_tests[rm] = {}
+        for test in results[rm]:
+            total_tests_performed += int(results[rm][test]["tests_performed"])
+            if results[rm][test]["errors"] != 0:
+                instructions_failed +=1
+                failed_tests[rm][test] = results[rm][test]
+                total_tests_failed += int(results[rm][test]["errors"])
+            else:
+                instructions_passed +=1
+    print("\n\n##################################")
+    print("######### TEST COMPLETED #########")
+    print("##################################")
+    print(f"TESTS PERFORMED: {total_tests_performed:,}")
+    print(f"TESTS FAILED: {total_tests_failed:,}\n\n")
+
+
+    print(f"A total of {total_tests_performed:,} tests for {instructions_failed + instructions_passed} total instructions using {len(results)} rounding modes was performed.")
+    print(f"{total_tests_failed:,} tests failed.\n")
+    if (total_tests_failed != 0):
+        print(f"The following instructions failed:")
+        for rm in failed_tests:
+            print(f"    {rm}")
+            for test in failed_tests[rm]:
+                print(f"       {test}: Tests performed = {int(failed_tests[rm][test]['tests_performed']):,}, tests failed = {int(failed_tests[rm][test]['errors']):,}")
+
+        print(f"\n For details, refer to the individual tests in: tests/{architecture}/<rounding mode>/<test>.txt\n")
+    print("########################################")
+    print("########################################")
+
 
 
 architecture = ""
@@ -33,33 +73,4 @@ if __name__ == "__main__":
     for arg in sys.argv:
         if arg[0:6] == "-arch=":
             architecture = arg[6:]
-
     get_results(architecture)
-        #Add more args if necessary
-            
-
-#         import os
-
-# def read_last_lines(file_path, number_of_lines=3):
-#     """Read the last n lines from a file."""
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         lines = file.readlines()
-#         return lines[-number_of_lines:]
-
-# def process_files_in_folder(folder_path):
-#     """Process each text file in the specified folder."""
-#     for filename in os.listdir(folder_path):
-#         if filename.endswith('.txt'):
-#             file_path = os.path.join(folder_path, filename)
-#             try:
-#                 last_lines = read_last_lines(file_path)
-#                 print(f"Last lines of {filename}:")
-#                 for line in last_lines:
-#                     print(line.strip())
-#                 print("\n")
-#             except Exception as e:
-#                 print(f"Error reading file {filename}: {e}")
-
-# # Replace 'your_folder_path' with the path of your folder
-# folder_path = 'your_folder_path'
-# process_files_in_folder(folder_path)
